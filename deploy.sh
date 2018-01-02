14850df6b65aa13e1a4c3951b708fd6393e84030
jeffrey95 <jeffreychiu95@gmail.com>
2018-01-02 15:45:08 +0800

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 tar -zcf $REPONAME.tar.gz _book
-scp $REPONAME.tar.gz -P $port git@$ip:$SCPDIR
+scp -P $port $REPONAME.tar.gz git@$ip:$SCPDIR
 rm $REPONAME.tar.gz
-echo $ip
-echo $port
 ssh -p $port git@$ip "cd $REPORTNAME;tar -zxf $REPONAME.tar.gz"
