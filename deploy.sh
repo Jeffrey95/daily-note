d1fe1732c35db48a9f48905640ff0bafe6d12f99
jeffrey95 <jeffreychiu95@gmail.com>
2018-01-02 22:59:55 +0800

    Track 3 files into repository.
    - modified .gitignore
    - modified .travis.yml
    - modified deploy.sh
    
    Auto commit by GitBook Editor

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 tar -zcf $REPONAME.tar.gz _book
 scp -P $port $REPONAME.tar.gz git@$ip:$SCPDIR
 rm $REPONAME.tar.gz
-ssh -p $port git@$ip "cd $REPORTNAME;tar -zxf $REPONAME.tar.gz"
+ssh -p $port git@$ip "cd $SCPDIR;if [ ! -d '$REPONAME' ];then mkdir $REPONAME;fi;tar -zxf $REPONAME.tar.gz;cp _book -R -u $REPONAME;rm $REPONAME.tar.gz;rm -rf _book;"
+
