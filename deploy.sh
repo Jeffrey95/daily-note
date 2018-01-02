6bc4c8a5f80135b63bc71324623f5d166294bd9d
jeffrey95 <jeffreychiu95@gmail.com>
2018-01-03 00:26:22 +0800

    Updates deploy.sh
    Auto commit by GitBook Editor

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 tar -zcf $REPONAME.tar.gz _book
 scp -P $port $REPONAME.tar.gz git@$ip:$SCPDIR
 rm $REPONAME.tar.gz
-ssh -p $port git@$ip "cd $SCPDIR;if [ ! -d '$REPONAME' ];then mkdir $REPONAME;fi;tar -zxf $REPONAME.tar.gz;cp _book -R -u $REPONAME;rm $REPONAME.tar.gz;rm -rf _book;"
+ssh -p $port git@$ip "cd $SCPDIR;if [ ! -d '$REPONAME' ];then mkdir $REPONAME;fi;tar -zxf $REPONAME.tar.gz;cp -R -u _book $REPONAME;rm $REPONAME.tar.gz;rm -rf _book;"
 
