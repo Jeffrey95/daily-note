a50320f79f96537be1bcb03f81cd6fdf020f7330
jeffrey95 <jeffreychiu95@gmail.com>
2018-02-17 22:45:29 +0800

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 tar -zcf $REPONAME.tar.gz _book
-scp -P $port $REPONAME.tar.gz git@$blog.qiujinfeng.com:$SCPDIR
+scp -P $port $REPONAME.tar.gz git@blog.qiujinfeng.com:$SCPDIR
 rm $REPONAME.tar.gz
-ssh -p $port git@$blog.qiujinfeng.com "cd $SCPDIR;if [ ! -d '$REPONAME' ];then mkdir $REPONAME;fi;tar -zxf $REPONAME.tar.gz;cp -R -u _book/* $REPONAME;rm $REPONAME.tar.gz;rm -rf _book;"
+ssh -p $port git@blog.qiujinfeng.com "cd $SCPDIR;if [ ! -d '$REPONAME' ];then mkdir $REPONAME;fi;tar -zxf $REPONAME.tar.gz;cp -R -u _book/* $REPONAME;rm $REPONAME.tar.gz;rm -rf _book;"
